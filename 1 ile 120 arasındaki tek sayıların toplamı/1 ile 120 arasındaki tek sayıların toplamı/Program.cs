00bb983d9449f8e442d25423f5a27bb881b2877e
Sefa Saraç <srcsfysf@gmail.com>
2024-07-22 15:57:14 +0300

    Add files via upload

---
+++ 1 ile 120 arasındaki tek sayıların toplamı/1 ile 120 arasındaki tek sayıların toplamı/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int baslangic = 3;
+        int bitis = 120;
+        int toplam = 0;
+
+        for (int i = baslangic; i < bitis; i += 2)
+        {
+            toplam += i;
+        }
+
+        Console.WriteLine($"1 ile 120 arasındaki tek sayıların toplamı: {toplam}");
+    }
+}
---
+++ 50 ile 150 arasındaki sayıların toplamı/50 ile 150 arasındaki sayıların toplamı/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int baslangic = 51;
+        int bitis = 150;
+        int toplam = 0;
+
+        for (int i = baslangic; i < bitis; i++)
+        {
+            toplam += i;
+        }
+
+        Console.WriteLine($"50 ile 150 arasındaki sayıların toplamı: {toplam}" );
+    }
+}
